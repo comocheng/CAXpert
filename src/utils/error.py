f803663157c07039ad35433783186668a84ae161
12Chao <xu.chao@northeastern.edu>
2024-08-13 08:54:56 -0700

    add new custom errors

--- src/utils/error.py
+++ src/utils/error.py
@@ -4,6 +4,20 @@ class AdsorbatesNotTaggedError(Exception):
 
     def __str__(self):
         return self.args[0]
+    
+class SurfaceNotTaggedError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def __str__(self):
+        return self.args[0]
+    
+class BulkTagError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def __str__(self):
+        return self.args[0]
 
 class TooManyAdsorbatesError(Exception):
     def __init__(self, message):
