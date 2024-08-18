4b7cf35bfb6d61fc51af9e7065689540c8cd9a8e
12Chao <xu.chao@northeastern.edu>
2024-08-18 15:18:27 -0700

    Add the corrected command for ML training

--- examples/ml_training_db.py
+++ examples/ml_training_db.py
@@ -13,7 +13,7 @@ os.makedirs('training_data/datasets', exist_ok=True)
 utils.train_test_val_split('training_data/ml_train.db', (0.8, 0.1, 0.1),('training_data/datasets/train.db','training_data/datasets/val.db', 'training_data/datasets/test.db'))
 
 date_today = '2024-08-15'
-command = f'time python /global/cfs/cdirs/m4126/xuchao/fairchem/main.py --mode train --config-yml ft/config.yml --checkpoint /global/cfs/cdirs/m4126/xuchao/ce_models/co_ni/ocp_train/fine_tuning/checkpoints/2024-06-22-17-21-20-co_ni_cov/best_checkpoint.pt --run-dir ft --identifier co_ni_cov --amp > ft/train.txt 2>&1'
+command = f'time python /global/cfs/cdirs/m4126/xuchao/fairchem/main.py --mode train --config-yml ft/config.yml --checkpoint /global/cfs/cdirs/m4126/xuchao/ce_models/co_ni/ocp_train/fine_tuning/checkpoints/2024-06-22-17-21-20-co_ni_cov/best_checkpoint.pt --run-dir ft --identifier co_h_ni_cov --amp > ft/train.txt 2>&1'
 add_fw([command],'/global/cfs/cdirs/m4126/xuchao/qm_calcs/methanation_ni/fws/my_launchpad.yaml', date_today)
 queue = '/global/cfs/cdirs/m4126/xuchao/caxpert/examples/fw_configs/my_qadapter_gpu.yaml'
 worker = '/global/cfs/cdirs/m4126/xuchao/caxpert/examples/fw_configs/my_fworker.yaml'
