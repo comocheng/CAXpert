6d41e2969f9369d115b74406bc472987ad367c26
12Chao <xu.chao@northeastern.edu>
2024-09-05 18:34:15 -0700

    write validated data to trajectories for the following DFT relax

--- examples/inf_dp.py
+++ examples/inf_dp.py
@@ -2,11 +2,12 @@ from caxpert.src.tasks.inference import mk_inf_db, MLInfDataProcess
 from ase.calculators.espresso import Espresso
 from caxpert.src.tasks.run_dft import ml_val
 import os
+from caxpert.src.tasks.gen_str import ml_val_db_to_trajs
 
-# mk_inf_db('init_structures.db','ft/ml_inf', 'ft/ml_inf.db')
+mk_inf_db('init_structures.db','ft/ml_inf', 'ft/ml_inf.db')
 mp = MLInfDataProcess('ft/ml_inf.db', ['co', 'h'], 'Ni', 4)
-# mp.plot_energy(output_fig='ft/ml_inf.html')
-# chs = mp.get_convex_hull()
+mp.plot_energy(output_fig='ft/ml_inf.html')
+chs = mp.get_convex_hull()
 
 # write your own code to choose the structures you want to use for validation and retrain the model
 h_only = mp.get_structures_to_validate([[0,0], [0.1,1]],10, cov_must_have=[(float(0), float(1))])
@@ -14,12 +15,12 @@ co_only = mp.get_structures_to_validate([[0.1,1], [0,0]],10, cov_must_have=[(flo
 h_co = mp.get_structures_to_validate([[0.3,1], [0.1,1]],10)
 ids_to_val = []
 for strs in [h_only, co_only, h_co]:
+# for strs in [h_co]:
     for v in strs.values():
         ids_to_val.append(v[1])
 
 calculated_ids = [int(i) for i in os.listdir('dft_relax')]
 calculated_ids.extend([int(i) for i in os.listdir('dft_relax_h_only')])
-
 ids_to_val = [i for i in ids_to_val if i not in calculated_ids]
 
 espresso_settings = {
@@ -38,9 +39,9 @@ espresso_settings = {
         'nspin': 2,
     },
     'electrons': {
-        'electron_maxstep': 200,
+        'electron_maxstep': 100,
         'mixing_mode': 'local-TF',
-        'mixing_beta': 0.5,
+        'mixing_beta': 0.2,
         'diagonalization': 'cg',
     },
 }
@@ -64,3 +65,6 @@ qe_calc = Espresso(
                 )
 
 ml_val(ids_to_val, 'ft/ml_inf.db', qe_calc, 'ft/ml_inf_dft_val.db')
+# ml_val([], 'ft/ml_inf.db', qe_calc, 'ft/ml_inf_dft_val.db', restart=True)
+
+ml_val_db_to_trajs('ft/ml_inf_dft_val.db')
